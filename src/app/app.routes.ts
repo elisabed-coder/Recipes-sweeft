a1a770675c5a009075ee09cbdf8ba5e7c8746321
Elisabed <134492535+elisabed-coder@users.noreply.github.com>
2025-02-01 02:01:24 +0400

    add get receipes function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'recipe', pathMatch: 'full' },
+  { path: 'recipe', component: RecipesComponent },
+];
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Recipe } from '../Models/recipe';
 
 @Component({
   selector: 'app-recipes',
   imports: [],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.scss'
+  styleUrl: './recipes.component.scss',
 })
 export class RecipesComponent {
+  http = inject(HttpClient);
+  recipes: Recipe[] = [];
 
+  private apiUrl = 'http://localhost:3001/recipes';
+
+  constructor() {
+    this.getRecipes().subscribe((data: Recipe[]) => {
+      console.log(data);
+      this.recipes = data;
+    });
+  }
+
+  getRecipes() {
+    return this.http.get<Recipe[]>(`${this.apiUrl}`);
+  }
 }
