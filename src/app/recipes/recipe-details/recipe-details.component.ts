2037c9ac1d52346b2f024ae603969bafb2e36dab
Elisabed <134492535+elisabed-coder@users.noreply.github.com>
2025-02-06 15:37:38 +0400

    add isfavourite function in recipe details too

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -5,45 +5,62 @@ import { RecipeService } from '../../Services/recipes.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { MatSnackBar } from '@angular/material/snack-bar'; // Import MatSnackBar
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FavoriteService } from '../../Services/favorite.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-details',
-  imports: [MatCardModule, MatButtonModule, CommonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MatIcon],
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss'],
 })
 export class RecipeDetailsComponent {
   recipeId!: string | null;
   recipe!: Recipe;
+  favoriteStatus: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private snackBar: MatSnackBar // Inject MatSnackBar
+    private snackBar: MatSnackBar,
+    private favoriteService: FavoriteService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.recipeId = params.get('id');
-      console.log('Recipe ID:', this.recipeId); // Debugging purpose
       if (this.recipeId) {
         this.recipeService.getRecipeById(this.recipeId).subscribe({
           next: (recipe) => {
             this.recipe = recipe;
-            console.log('Recipe Details:', this.recipe); // Debugging purpose
+            if (this.recipe.id) {
+              this.favoriteStatus = this.favoriteService.isFavorite(
+                this.recipe.id
+              );
+            }
           },
           error: (err) => {
             const errorMessage =
               err.message || 'An error occurred while fetching recipe details';
             this.snackBar.open(errorMessage, 'Close', {
-              duration: 5000, // Show for 5 seconds
-              panelClass: ['error-snackbar'], // Optional class for styling
+              duration: 5000,
+              panelClass: ['error-snackbar'],
             });
           },
         });
       }
     });
+  }
+
+  isFavorite(recipeId: string | undefined): boolean {
+    return recipeId ? this.favoriteService.isFavorite(recipeId) : false;
+  }
+
+  toggleFavorite(recipe: Recipe) {
+    if (recipe.id) {
+      this.favoriteService.toggleFavorite(recipe.id);
+    }
   }
 
   updateRecipe() {
